341669a2686bed3aa15e6ee316818f4d843cdb94
Diya Boudraa <diya.boudraa@etu.unistra.fr>
2025-05-14 09:43:59 +0200

    Package armure :     classes Equipement, Arme et Armure

--- src/equipement/Equipement.java
+++ src/equipement/Equipement.java
@@ -1,4 +1,20 @@
 package equipement;
 
 public abstract class Equipement {
+    private String m_nom;
+    protected int[] m_modifStat = new int[5];
+
+    public Equipement(String nom) {
+        m_nom = nom;
+        m_modifStat[0] = 0;
+        m_modifStat[1] = 0;
+        m_modifStat[2] = 0;
+        m_modifStat[3] = 0;
+        m_modifStat[4] = 0;
+    }
+
+    public int[] getModifStat(){
+        return m_modifStat;
+    }
+
 }
--- src/equipement/arme/Arme.java
+++ src/equipement/arme/Arme.java
@@ -2,4 +2,57 @@ package equipement.arme;
 import equipement.Equipement;
 
 public class Arme extends Equipement {
+    private int m_nbDesDeg;
+    private int m_degats;
+    private int m_portee;
+    private TypeCaC m_type;
+
+    public Arme() {
+        super("nomArme");
+        m_nbDesDeg = 1;
+        m_degats = 1;
+        m_portee = 1;
+        m_type = TypeCaC.COURANTE;
+    }
+
+    public Arme(String nom, int nbDes, int nbFaces, int portee){
+        super(nom);
+        m_nbDesDeg = nbDes;
+        m_degats = nbFaces;
+        m_portee = portee;
+        m_type = null;
+    }
+
+    public Arme(String nom, int nbDes, int nbFaces, TypeCaC type){
+        super(nom);
+        m_nbDesDeg = nbDes;
+        m_degats = nbFaces;
+        m_portee = 1;
+        m_type = type;
+        if(type == TypeCaC.GUERRE){
+            m_modifStat[3] = -2;
+            m_modifStat[1] = 4;
+        }
+    }
+
+    public int getPortee(){
+        return m_portee;
+    }
+
+
+    public int infligerDegats() {
+        int degats = 0;
+        for(int i = 0; i<m_nbDesDeg;i++){
+            degats += lancerDes(m_degats);
+            //faudra juste import le package Des quand on aura merge
+        }
+
+        return degats;
+    }
+
+    public int quelleStat(){
+        return this.m_portee>1 ? 2 : 1;
+        //Si portée sup à 1 donc arme à distance alors la statistique qui importe
+        //c'est la dextérité, si c'est CàC alors ce serait force
+    }
 }
